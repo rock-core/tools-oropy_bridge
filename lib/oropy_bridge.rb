72329010bc274051051ab2aff1a0df4096348862
Felix Rehrmann <felix.rehrmann@dfki.de>
2013-05-07 09:43:33 +0200

    Initial commit

---
+++ lib/oropy_bridge.rb
@@ -0,0 +1,10 @@
+require 'oropy_bridge/oropy_bridge'
+
+# The toplevel namespace for oropy_bridge
+#
+# You should describe the basic idea about oropy_bridge here
+require 'utilrb/logger'
+module OropyBridge
+    extend Logger::Root('OropyBridge', Logger::WARN)
+end
+
