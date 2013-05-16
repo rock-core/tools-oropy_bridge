81fab79cb19a116d20271e8b4a84c014193b6c27
Felix Rehrmann <felix.rehrmann@dfki.de>
2013-05-16 14:08:50 +0200

    test scripts to test the ruby interface sing msgpack

---
+++ test/test_msg_if.rb
@@ -0,0 +1,120 @@
+#!/usr/bin/env ruby
+
+require 'msgpack'
+require '../ruby/lib/oropy_bridge/oropy_bridge'
+
+r_caller, w_msg = IO.pipe
+r_msg, w_caller = IO.pipe
+
+handler = OropyBridge::OrocosRb.new
+cal = OropyBridge::Caller.new r_caller, w_caller, handler
+
+calT = Thread.new { while cal.process; end; w_caller.close }
+
+#backT = Thread.new {
+#    unpacker = MessagePack::Unpacker.new(r_msg)
+#    while 1
+#        begin
+#            unpacker.each do |obj|
+#                if obj[0] == "OtherError"
+#                    puts "#{obj[0]} : #{obj[1][0]}(#{obj[1][1]})"
+#                    obj[1][2].each do |l|
+#                        puts l
+#                    end
+#                elsif obj[0][-5,5] ==  "Error"
+#                    puts "#{obj[0]} : #{obj[1][0]}"
+#                    obj[1][1].each do |l|
+#                        puts l
+#                    end
+#                else
+#                    puts "response-cmd: #{obj[0]}"
+#                    puts "response-arg: #{obj[1]}"
+#                end
+#            end
+#        rescue EOFError => e
+#            break
+#        end
+#    end
+#}
+def wait_for_reply(cmd, r_msg)
+    unpacker = MessagePack::Unpacker.new(r_msg)
+    while 1
+        begin
+            unpacker.each do |obj|
+                if obj[0] == "OtherError"
+                    puts "#{obj[0]} : #{obj[1][0]}(#{obj[1][1]})"
+                    obj[1][2].each do |l|
+                        puts l
+                    end
+                    break
+                elsif obj[0][-5,5] ==  "Error"
+                    puts "#{obj[0]} : #{obj[1][0]}"
+                    obj[1][1].each do |l|
+                        puts l
+                    end
+                    break
+                elsif obj[0] == cmd
+                    return obj[1]
+                end
+            end
+        rescue EOFError => e
+            break
+        end
+    end
+    Nil
+end
+
+def send_cmd(cmd, r_msg, packer)
+    packer.write(cmd).flush()
+    reply = wait_for_reply(cmd[0],r_msg)
+    puts "--- got reply for #{cmd[0]}"
+    reply
+end
+
+packer = MessagePack::Packer.new(w_msg)
+
+deploy_cmd = [ "deploy", [{"statistics::CumulativeTask" => "stats"}]]
+send_cmd(deploy_cmd,r_msg,packer)
+
+
+a_new_port = Hash.new
+a_new_port["portname"] = "rbs1"
+a_new_port["type"] = "/base/samples/RigidBodyState"
+a_new_port["slice"] = "position"
+a_new_port["vectorIdx"] = 0
+a_new_port["period"] = 0.1
+a_new_port["useTimeNow"] = true
+add_port_cmd = [ "addPort", [ "/stats", a_new_port]]
+send_cmd(add_port_cmd,r_msg,packer)
+
+config = Hash.new
+config["debug_conversion"] = true
+apply_config_cmd = [ "apply_config", ["/stats", config]]
+send_cmd(apply_config_cmd,r_msg,packer)
+
+send_cmd(["configure",["/stats"]],r_msg,packer)
+send_cmd(["start",["/stats"]],r_msg,packer)
+
+puts "Press Enter to send data!"
+gets
+
+write_cmd = ["write_vector",["/stats","rbs1_raw",[0.0,2.0,3.0]]]
+read_cmd = ["read",["/stats","stats_0"]]
+puts send_cmd(read_cmd,r_msg,packer)
+sleep(0.1)
+(1..10).each do |idx|
+    send_cmd(write_cmd,r_msg,packer)
+    sleep(0.1)
+    puts send_cmd(read_cmd,r_msg,packer)
+end
+
+send_cmd(["stop",["/stats"]],r_msg,packer)
+send_cmd(["cleanup",["/stats"]],r_msg,packer)
+
+stop_deployments_cmd = ["stop_deployments"]
+send_cmd(stop_deployments_cmd,r_msg,packer)
+
+w_msg.close
+
+calT.join
+#backT.join
--- test/test_orocos_rb_server.rb
+++ test/test_orocos_rb_server.rb
@@ -1,14 +1,14 @@
 require '../ruby/lib/oropy_bridge/oropy_bridge'
 
-server = OropyBridge::OrocosRbServer.new("statistics::CumulativeTask" => "stats")
+handler = OropyBridge::OrocosRb.new
 
-server.deploy 
+handler.deploy("statistics::CumulativeTask" => "stats")
 
 config = Hash.new
 config["debug_conversion"] = true
 
 puts "known tasks:"
-server.tasks.each do |k,_|
+handler.tasks.each do |k,_|
     puts k
 end
 
@@ -20,40 +20,40 @@ a_new_port["vectorIdx"] = 0
 a_new_port["period"] = 0.1
 a_new_port["useTimeNow"] = true
 
-server.addPort("/stats",a_new_port)
+handler.addPort("/stats",a_new_port)
 
-server.apply_config("/stats", config)
+handler.apply_config("/stats", config)
 
-server.configure("/stats")
+handler.configure("/stats")
 
 puts
 puts "Task  Type  Orocos_Type"
 puts "--- Input ports ---"
-server.tasks["/stats"].each_input_port do |p|
+handler.tasks["/stats"].each_input_port do |p|
     puts "#{p.name}  #{p.type_name}  #{p.orocos_type_name}"
 end
 
 puts "\n--- Output ports ---"
-server.tasks["/stats"].each_output_port do |p|
+handler.tasks["/stats"].each_output_port do |p|
     puts "#{p.name}  #{p.type_name}  #{p.orocos_type_name}"
 end
 puts
 
-server.start("/stats")
+handler.start("/stats")
 
 puts "Press Enter to go on!"
 gets
 
 (1..10).each do |idx|
-    server.write_vector("/stats","rbs1_raw",[0.0,2.0,3.0])
+    handler.write_vector("/stats","rbs1_raw",[0.0,2.0,3.0])
     sleep(0.1)
     puts "debug (#{idx}):"
-    puts server.read_vector("/stats","debug_0")
+    puts handler.read_vector("/stats","debug_0")
     puts "stats:"
-    puts server.read("/stats", "stats_0")
+    puts handler.read("/stats", "stats_0")
 end
 
-server.stop("/stats")
-server.cleanup("/stats")
+handler.stop("/stats")
+handler.cleanup("/stats")
 
-server.stop_deployments
+handler.stop_deployments
