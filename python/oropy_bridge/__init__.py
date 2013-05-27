2d747ab58ed2be3c7d60241b7f4259f72c004956
Felix Rehrmann <felix.rehrmann@dfki.de>
2013-05-27 14:35:54 +0200

    added license information into manifest and reworked documentation

--- python/oropy_bridge/__init__.py
+++ python/oropy_bridge/__init__.py
@@ -1,42 +1,42 @@
-''' Using orogen components from within python.
+'''Using orogen components from within python.
 
 This module allows to deploy orogen components and use them similiar to the ruby interface.
 
 Usage is:
 
->>> import oropy_bridge
->>> 
->>> client = oropy_bridge.Client()#(ruby_cmd=["oropy_server"]) # --log
->>> 
->>> started_tasks = client.deploy({"statistics::CumulativeTask" : "stats"})
->>> 
->>> config = { "debug_conversion" : True, "aggregator_max_latency" : 0.1}
->>> 
->>> a_new_port = {
->>>         "portname" : "rbs1",
->>>         "type" : "/base/samples/RigidBodyState",
->>>         "slice" : "position",
->>>         "vectorIdx" : 0,
->>>         "period" : 0.1,
->>>         "useTimeNow" : True
->>> }
->>> 
->>> client.operation("/stats","addPort",a_new_port)
->>> client.apply_config("/stats",config)
->>> client.configure("/stats")
->>> client.start("/stats")
->>> 
->>> for i in xrange(50):
->>>     client.write_vector("/stats","rbs1_raw",[float(i),2.0,3.0])
->>>     time.sleep(0.1)
->>>     print "stats:"
->>>     stats = client.read("/stats","stats_0")
->>>     if stats: print "(%i) t=%.6f mean: %f"%(stats.get("n"),stats.get("time"),stats.get("mean")[0])
->>>     else: print None
->>> 
->>> client.stop("/stats")
->>> client.cleanup("/stats")
->>> client.stop_deployments()
+    import oropy_bridge
+    
+    client = oropy_bridge.Client()#(ruby_cmd=["oropy_server"]) # --log
+    
+    started_tasks = client.deploy({"statistics::CumulativeTask" : "stats"})
+    
+    config = { "debug_conversion" : True, "aggregator_max_latency" : 0.1}
+    
+    a_new_port = {
+            "portname" : "rbs1",
+            "type" : "/base/samples/RigidBodyState",
+            "slice" : "position",
+            "vectorIdx" : 0,
+            "period" : 0.1,
+            "useTimeNow" : True
+    }
+    
+    client.operation("/stats","addPort",a_new_port)
+    client.apply_config("/stats",config)
+    client.configure("/stats")
+    client.start("/stats")
+    
+    for i in xrange(50):
+        client.write_vector("/stats","rbs1_raw",[float(i),2.0,3.0])
+        time.sleep(0.1)
+        print "stats:"
+        stats = client.read("/stats","stats_0")
+        if stats: print "(%i) t=%.6f mean: %f"%(stats.get("n"),stats.get("time"),stats.get("mean")[0])
+        else: print None
+    
+    client.stop("/stats")
+    client.cleanup("/stats")
+    client.stop_deployments()
 
 '''
 from oropy_bridge import *
--- python/oropy_bridge/oropy_bridge.py
+++ python/oropy_bridge/oropy_bridge.py
@@ -1,119 +1,117 @@
 try:
     import msgpack
 except ImportError, e:
-    print "\x1b[31;1mInstall msgpack for python manually:"
-    print "   [apt-get install python-pip]"
-    print "    pip install --user msgpack-python\x1b[0m"
+    print >> stderr, "\x1b[31;1mInstall msgpack for python manually:"
+    print >> stderr, "   [apt-get install python-pip]"
+    print >> stderr, "    pip install --user msgpack-python\x1b[0m"
     raise e
-import time
-import socket
 import subprocess
 from sys import stderr
 
-class OrocosRb:
+class OrocosRb(object):
     ''' Provides all the methods to interact with orogen tasks.'''
 
     def __init__(self, cmd_forwarder):
-        ''' Initialization.
+        '''Initialization.
 
-            Args:
-                cmd_forwarder: A class with a *process* method that takes a msg and
-                    forwards it to the serving entity and returns the reply.
+        Args:
+            cmd_forwarder: A class with a *process* method that takes a msg and
+                forwards it to the serving entity and returns the reply.
         '''
         self.cmder = cmd_forwarder
 
     def deploy(self, deployments):
-        ''' Deploy tasks.
+        '''Deploy tasks.
 
-            Args:
-                deployments: The deployments that contain the tasks. Takes deployment names
-                    , default deplyoments and options.
+        Args:
+            deployments: The deployments that contain the tasks. Takes deployment names
+                , default deplyoments and options.
 
-            >>> .deploy(["deplyomentA", "deploymentB", { "default::Task" : "taskC", "valgrind" : True }]
+        .deploy(["deplyomentA", "deploymentB", { "default::Task" : "taskC", "valgrind" : True }]
 
-            Returns:
-                The list of tasks started with the deployments.
+        Returns:
+            The list of tasks started with the deployments.
         '''
         msg = ["deploy",[deployments]]
         return self.cmder.process(msg)
 
     def stop_deployments(self):
-        ''' Stop all deployments. Also stops the ruby deployment thread.'''
+        '''Stop all deployments. Also stops the ruby deployment thread.'''
         msg = ["stop_deployments",[]]
         return self.cmder.process(msg)
 
     def apply_config(self, task, config):
-        ''' Applies a configuration to a task. 
+        '''Applies a configuration to a task. 
 
-            Args:
-                task: The task to apply the configuration to.
-                config: A dictionary with the properies of the task as keys.
-                    Only properties meant to be set need to be inside the dictionary.
+        Args:
+            task: The task to apply the configuration to.
+            config: A dictionary with the properies of the task as keys.
+                Only properties meant to be set need to be inside the dictionary.
         '''
         msg = ["apply_config",[task,config]]
         return self.cmder.process(msg)
 
     def configure(self, task):
-        ''' Configure a *task* (like task.configure).'''
+        '''Configure a *task* (like task.configure).'''
         msg = ["configure",[task]]
         return self.cmder.process(msg)
 
     def start(self, task): 
-        ''' Start a *task* (like task.start).'''
+        '''Start a *task* (like task.start).'''
         msg = ["start",[task]]
         return self.cmder.process(msg)
 
     def operation(self, task, operation, *args):
-        ''' Call an operation of a task.
+        '''Call an operation of a task.
 
-            Args:
-                task: Name of the task which has the operation.
-                operation: Name of the operation.
-                *args: Argument list.
-            
-            Returns:
-                The return value of the operation.
+        Args:
+            task: Name of the task which has the operation.
+            operation: Name of the operation.
+            *args: Argument list.
+        
+        Returns:
+            The return value of the operation.
         '''
         msg = ["operation",[task,operation,args]]
         return self.cmder.process(msg)
 
     def write(self, task, portname, data):
-        ''' Write data to a port.
+        '''Write data to a port.
 
-            Args:
-                task: Name of the task that has the port.
-                portname: Name of the port.
-                data: Data to write to the port. A dictionary that matches the 
-                    structure of the porttpye.
+        Args:
+            task: Name of the task that has the port.
+            portname: Name of the port.
+            data: Data to write to the port. A dictionary that matches the 
+                structure of the porttpye.
         '''
         msg = ["write",[task,portname,data]]
         return self.cmder.process(msg)
     
     def write_vector(self, task, portname, data):
-        ''' Writes a vector to a port of type base::VectorXd.'''
-        msg = ["write_vector",[task,portname,data]]
+        '''Writes a vector to a port of type base::VectorXd.'''
+        msg = ["write_vector", [task, portname, data]]
         return self.cmder.process(msg)
     
     def read(self, task, portname, new_data=False):
-        ''' Read data from a port.
-            
-            Args:
-                task: Name of the task that has the port.
-                portname: Name of the port.
-                new_data: If True read only new data.
+        '''Read data from a port.
+ 
+        Args:
+            task: Name of the task that has the port.
+            portname: Name of the port.
+            new_data: If True read only new data.
 
-            Returns:
-                The data from the port or None if there were no data.
+        Returns:
+            The data from the port or None if there were no data.
         '''
-        msg = ["read",[task,portname,new_data]]
+        msg = ["read", [task,portname, new_data]]
         return self.cmder.process(msg)
 
     def read_vector(self, task, portname, new_data=False):
-        msg = ["read_vector",[task,portname,new_data]]
+        msg = ["read_vector", [task, portname, new_data]]
         return self.cmder.process(msg)
 
     def connect(self, from_task, from_port, to_task, to_port):
-        ''' Connect two ports (not tested). 
+        '''Connect two ports (not tested). 
             
             like from_task.from_port.connect_to to_task.to_port
         '''
@@ -130,18 +128,23 @@ class OrocosRb:
         msg = ["cleanup",[task]]
         return self.cmder.process(msg)
 
+    def sleep(self, seconds):
+        '''Sleep for seconds.'''
+        msg =["sleep",[seconds]]
+        return self.cmder.process(msg)
 
-class Forwarder:
-    ''' Forward the command message from the class OrocosRb to another entity that
-        executes the commands.
+
+class Forwarder(object):
+    '''Forward the command message from the class OrocosRb to another entity that
+    executes the commands.
     '''
 
     def __init__(self, reader, writer=None):
-        ''' Initialization.
+        '''Initialization.
 
-            Args:
-                reader: The replies are expected on that line.
-                writer: The commands are wirten to this stream.
+        Args:
+            reader: The replies are expected on that line.
+            writer: The commands are wirten to this stream.
         '''
         self.reader = reader
         if writer:
@@ -149,7 +152,7 @@ class Forwarder:
         else:
             self.writer = reader
         
-    def process_incoming(self, timeout_s = None):
+    def process_incoming(self):
         ''' Process incoming message from the reader. Uses msgpack to unpack
             the messages.
 
@@ -157,11 +160,9 @@ class Forwarder:
                 The reply.
         '''
         unpacker = msgpack.Unpacker()
-        start = time.clock()
-        while True: #not timeout_s or time.clock() - start < timeout_s:
+        while True:
             data = self.reader.read(1)
             if not data:
-                print >> stderr, "no data"
                 break
             unpacker.feed(data)
             for o in unpacker:
@@ -169,13 +170,13 @@ class Forwarder:
         return None
 
     def process(self, msg):
-        ''' Send messages and wait for the reply.
+        '''Send messages and wait for the reply.
 
-            Args:
-                msg: The command message to forward.
+        Args:
+            msg: The command message to forward.
 
-            Returns:
-                The reply or None if an error occured.
+        Returns:
+            The reply or None if an error occured.
         '''
         self.writer.write(msgpack.packb(msg))
         reply = self.process_incoming()
@@ -186,27 +187,29 @@ class Forwarder:
             return reply[1]
         elif reply[0][-5:] == "Error":
             err_data = reply[1]
-            print "\x1b[31;1mError:%s, %s\x1b[0m"%(err_data[0],err_data[1])
+            print >> stderr, "\x1b[31;1mError:%s, %s\x1b[0m"%(err_data[0],err_data[1])
             for l in err_data[2]:
-                print l
-            print
+                print >> stderr, l
+            print >> stderr
             return None
 
 
 class Client(OrocosRb):
     ''' The client to connect to the ruby server to access orocor.rb. '''
 
-    def __init__(self, ruby_cmd="oropy_server"):
-        ''' Initializing the client.
+    def __init__(self, ruby_cmd="oropy_server", batch_mode=False):
+        '''Initializing the client.
 
-            Start the ruby orocos serving side, connect to it and accept commands.
-            Commands the class OrocosRb.
+        Start the ruby orocos serving side, connect to it and accept commands.
+        Commands the class OrocosRb.
 
-            Args:
-                ruby_cmd: the command to run the ruby server.
+        Args:
+            ruby_cmd: the command to run the ruby server.
+            batch_mode: run in batch mode, collect commands and execute at once.
+                Adds "--batch" option to the ruby_cmd.
 
-            A ruby script is started in another process. Both process are connected by a
-            pipe. The data serialization is done with msgpack.
+        A ruby script is started in another process. Both process are connected by a
+        pipe. The data serialization is done with msgpack.
         '''
         slave = subprocess.Popen(ruby_cmd,stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=None)
         self.cmder = Forwarder(slave.stdout, slave.stdin)
